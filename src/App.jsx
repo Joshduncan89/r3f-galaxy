8f47485c0a0b820ed62939a4813b6dbd75d91b5d
Joshduncan89 <51183094+Joshduncan89@users.noreply.github.com>
2024-02-28 16:29:39 -0500

    add background, set up moon

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,74 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { Box, Environment, OrbitControls, useTexture, useCubeTexture } from "@react-three/drei";
+import { useControls } from "leva";
+import { Box, Environment, OrbitControls, useTexture, useCubeTexture, PerspectiveCamera } from "@react-three/drei";
 import Cube from "./Cube";
 
-const Background = () => {
-  // const skyboxTexture = useCubeTexture(
-  //   ["heather_ft.jpg", "heather_bk.jpg", "heather_up.jpg", "heather_dn.jpg", "heather_lf.jpg", "heather_rt.jpg"],
-  //   { path: "/background/" }
-  // );
-
-  //   const [texUp,texDown,texLeft,texRight,texFr,texBk] = useTexture('/background/heather_up.jpg','/background/heather_dn.jpg','/background/heather_lf.jpg','/background/heather_rt.jpg','/background/heather_ft.jpg','/background/heather_bk.jpg')
-
-  const { scene } = useThree(); // Access the Three.js scene instance
-  const skyboxTexture = useCubeTexture(
-    ["heather_rt.jpg", "heather_lf.jpg", "heather_up.jpg", "heather_dn.jpg", "heather_ft.jpg", "heather_bk.jpg"],
-    { path: "/background/" }
-  );
-
-  // Update the background of the scene with the skybox texture
-  scene.background = skyboxTexture;
-
-  return null;
-};
-
 const Scene = () => {
-  const boxRef = useRef();
-  useFrame((state, delta) => {
-    boxRef.current.rotation.y += 0.02;
+  const { camera, gl } = useThree();
+
+  // Create a ref for the camera
+  const controls = useRef();
+
+  useEffect(() => {
+    const handleControlsChange = () => {
+      console.log("Camera Position:", camera.position);
+      console.log("Camera Rotation:", camera.rotation);
+    };
+
+    controls.current.addEventListener("change", handleControlsChange);
+
+    return () => {
+      controls.current.removeEventListener("change", handleControlsChange);
+    };
+  }, [camera]);
+
+  useFrame(() => {
+    controls.current.update(); // Make sure to update controls in the animation loop
   });
 
   return (
     <>
-      <Box ref={boxRef} args={[1, 1, 1]} rotation={[0.5, 0, 0]}>
-        <meshNormalMaterial />
-      </Box>
-      <ambientLight />
+      <PerspectiveCamera makeDefault position={[-25, -15, 20]} fov={70} near={1} far={1000} />
+      <OrbitControls ref={controls} args={[camera, gl.domElement]} />
+      <Environment files='https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/4k/dikhololo_night_4k.hdr' background />
+      <hemisphereLight intensity={0.5} color='white' groundColor='black' />
     </>
+  );
+};
+
+const Moon = () => {
+  const moonRef = useRef();
+
+  const moonPosition = useControls({
+    x: { value: 30, min: -50, max: 50 },
+    y: { value: 0, min: -50, max: 50 },
+    z: { value: 10, min: -50, max: 50 },
+  });
+
+  // Update moon rotation using useFrame
+  useFrame(() => {
+    // Rotate the moon over time
+    moonRef.current.rotation.y += 0.005;
+  });
+
+  return (
+    <mesh ref={moonRef} position={moonPosition}>
+      {" "}
+      {/* Adjust position based on your scene */}
+      {/* Create the moon geometry and material */}
+      <sphereGeometry args={[5, 32, 32]} /> {/* Adjust radius, segments, and rings */}
+      <meshBasicMaterial color={0xffffff} /> {/* Adjust moon color */}
+    </mesh>
   );
 };
 
 const App = () => {
   return (
-    <Canvas camera={{ fov: 70, position: [0, 0, 10] }} style={{ width: "100%", height: "100%" }}>
-      <hemisphereLight intensity={0.5} color='white' groundColor='black' />
-      <OrbitControls />
-      <Background />
+    <Canvas style={{ width: "100%", height: "100%" }}>
       <Scene />
+      <OrbitControls />
+      <Moon />
     </Canvas>
   );
 };
